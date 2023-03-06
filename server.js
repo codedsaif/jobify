41d4ad8deda1f65a89b99d29f0648122ed1d6c96
S-a-i-f-A-l-i <saifali27906@gmail.com>
2023-03-06 13:35:50 +0530

    not-found & error-handler middleware added

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from "express";
-
 const app = express();
 
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
 app.get("/", (req, res) => {
+  throw new Error("error");
   res.send("Welcome!");
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
