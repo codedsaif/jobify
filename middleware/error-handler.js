bd1883d37f7b506507ac15e05b8540ea885b2d79
S-a-i-f-A-l-i <saifali27906@gmail.com>
2023-03-17 18:53:28 +0530

    Realated to Register approx All error done

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,9 @@
 import { StatusCodes } from "http-status-codes";
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    message: "Something went wrong, try again later",
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || "Something went wrong, try again later",
   };
   if (err.name === "ValidationError") {
     defaultError.statusCode = StatusCodes.BAD_REQUEST;
@@ -11,6 +11,12 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     defaultError.message = Object.values(err.errors)
       .map((item) => item.message)
       .join(",");
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.message = `${Object.keys(
+      err.keyValue
+    )} field has to be unique`;
   }
   // return res.status(defaultError.statusCode).json({ msg: err });
   return res
