a9823eb325c8f14f9a0138f2b26a95b155c28ab0
S-a-i-f-A-l-i <saifali27906@gmail.com>
2023-03-22 11:20:57 +0530

    desktop sidebar done

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Wrapper from "../assets/wrappers/SmallSidebar";
 import { useAppContext } from "../context/appContext";
-import links from "../utils/links";
-import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
 import Logo from "./Logo";
 import { FaTimes } from "react-icons/fa";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+
 const SmallSidebar = () => {
   const { showSidebar, toggleSidebar } = useAppContext();
   return (
@@ -21,24 +21,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { text, path, id, icon } = link;
-              return (
-                <NavLink
-                  to={path}
-                  className={({ isActive }) =>
-                    isActive ? "nav-link active" : "nav-link"
-                  }
-                  key={id}
-                  onClick={toggleSidebar}
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggleSidebar} />;
         </div>
       </div>
     </Wrapper>
